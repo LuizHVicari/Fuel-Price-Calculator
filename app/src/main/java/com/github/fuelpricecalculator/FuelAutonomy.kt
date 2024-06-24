6771887a411c9f4a0902b1cf81bc965e1aae0c51
LuizHVicari <luizhenrique.vicari@gmail.com>
2024-06-24 19:24:20 -0300

    main project

---
+++ app/src/main/java/com/github/fuelpricecalculator/FuelAutonomy.kt
@@ -0,0 +1,20 @@
+package com.github.fuelpricecalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FuelAutonomy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fuel_autonomy)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
