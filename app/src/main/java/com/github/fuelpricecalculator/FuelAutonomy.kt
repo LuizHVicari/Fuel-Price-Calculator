549b42d66c2b7be46a552f5fca50c31ddbeac846
LuizHVicari <luizhenrique.vicari@gmail.com>
2024-06-24 19:24:25 -0300

    main project

--- app/src/main/java/com/github/fuelpricecalculator/FuelAutonomy.kt
+++ app/src/main/java/com/github/fuelpricecalculator/FuelAutonomy.kt
@@ -1,20 +1,45 @@
 package com.github.fuelpricecalculator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.github.fuelpricecalculator.databinding.ActivityFuelAutonomyBinding
+import java.util.Locale
 
 class FuelAutonomy : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFuelAutonomyBinding
+    private var valueFuel1 = 0.0f
+    private var valueFuel2 = 0.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_fuel_autonomy)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityFuelAutonomyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        valueFuel1 = intent.getFloatExtra("valueFuel1", 0.0f)
+        valueFuel2 = intent.getFloatExtra("valueFuel2", 0.0f)
+
+        val distancePerCost1 = 1.0 / valueFuel1
+        val distancePerCost2 = 1.0 / valueFuel2
+
+        if (distancePerCost1 > distancePerCost2) {
+            binding.llFuel1.layoutParams.width = dpToPixel(300)
+            binding.llFuel2.layoutParams.width = dpToPixel((300 * distancePerCost2 / distancePerCost1).toInt())
+
+        } else {
+            binding.llFuel2.layoutParams.width = dpToPixel(300)
+            binding.llFuel1.layoutParams.width = dpToPixel((300 * distancePerCost1 / distancePerCost2).toInt())
         }
+
+        binding.tvFuel1.text = getString(R.string.fuel_1_autonomy, String.format(Locale.getDefault(), "%.2f", distancePerCost1 * 100.0))
+        binding.tvFuel2.text = getString(R.string.fuel_2_autonomy, String.format(Locale.getDefault(), "%.2f", distancePerCost2 * 100.0))
+    }
+
+    private fun dpToPixel(dp: Int): Int {
+        val scale = resources.displayMetrics.density
+        val pixels = (dp * scale + 0.5f).toInt()
+        return pixels
     }
 }
--- app/src/main/java/com/github/fuelpricecalculator/MainActivity.kt
+++ app/src/main/java/com/github/fuelpricecalculator/MainActivity.kt
@@ -1,20 +1,145 @@
 package com.github.fuelpricecalculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.github.fuelpricecalculator.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputLayout
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var gasolineLanguages : Array<String>
+    private lateinit var ethanolLanguages : Array<String>
+    private lateinit var naturalGasLanguages : Array<String>
+    private lateinit var customLanguages : Array<String>
+    private var valueFuel1 = 0.0f
+    private var valueFuel2 = 0.0f
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        gasolineLanguages = arrayOf("Gasoline", "Gasolina")
+        ethanolLanguages = arrayOf("Ethanol", "Etanol")
+        naturalGasLanguages = arrayOf("Natural Gas", "Gás Natural")
+        customLanguages = arrayOf("Custom", "Personalizado")
+
+
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+
+        val systemLanguage = Locale.getDefault().language
+        val spinnerArray = if (systemLanguage == "pt"){
+            R.array.fuels_pt
+        } else {
+            R.array.fuels_en
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            spinnerArray ,
+            R.layout.spinner_item).also {
+                adapter ->
+                adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+                binding.spFuels1.adapter = adapter
+                binding.spFuels2.adapter = adapter
+        }
+
+        binding.btCalculate.setOnClickListener{
+            btCalculateOnClickListener()
+        }
+
+        binding.btShowDifference.setOnClickListener {
+            btShowDifferenceOnClick()
         }
     }
+
+    private fun btShowDifferenceOnClick() {
+        val intent = Intent(this, FuelAutonomy::class.java)
+        val b = Bundle()
+        b.putFloat("valueFuel1", valueFuel1)
+        b.putFloat("valueFuel2", valueFuel2)
+        intent.putExtras(b)
+        startActivity(intent)
+    }
+
+    private fun btCalculateOnClickListener(){
+        val spinnerItem1 = binding.spFuels1.selectedItem.toString()
+        val spinnerItem2 = binding.spFuels2.selectedItem.toString()
+
+        val customFuelConsumption1 = binding.tiConsumption1.editText?.text.toString().toFloatOrNull()
+        val customFuelConsumption2 = binding.tiConsumption2.editText?.text.toString().toFloatOrNull()
+
+        val cost1 = binding.tiCost1.editText?.text.toString().toFloatOrNull()
+        val cost2 = binding.tiCost2.editText?.text.toString().toFloatOrNull()
+
+        if (!validateConsumption(spinnerItem1, customFuelConsumption1, binding.tiConsumption1)
+            ||
+            !validateConsumption(spinnerItem2, customFuelConsumption2, binding.tiConsumption2)
+            ||
+            !validateCost(cost1, binding.tiCost1) || !validateCost(cost2, binding.tiCost2)
+            ) {
+            return
+        }
+
+        val fuelConsumption1 = customFuelConsumption1 ?: getFuelConsumption(spinnerItem1)
+        val fuelConsumption2 = customFuelConsumption2 ?: getFuelConsumption(spinnerItem2)
+
+        valueFuel1 = ((1.0 / fuelConsumption1) * cost1!!).toFloat()
+        valueFuel2 = ((1.0 / fuelConsumption2) * cost2!!).toFloat()
+
+        if (valueFuel1 > valueFuel2) {
+            binding.tvResult.text = getString(R.string.the_best_fuel_option_is_s, "2")
+        } else if (valueFuel1 < valueFuel2) {
+            binding.tvResult.text = getString(R.string.the_best_fuel_option_is_s, "1")
+        } else {
+            binding.tvResult.text = getString(R.string.fuel_option_does_not_matter)
+        }
+        binding.tvResult.visibility = View.VISIBLE
+        binding.btShowDifference.visibility = View.VISIBLE
+        binding.btShowDifference.isClickable = true
+
+    }
+
+    private fun getFuelConsumption(spinnerValue: String): Float{
+        return if (gasolineLanguages.contains(spinnerValue)) {
+            15.0f
+        } else if (ethanolLanguages.contains(spinnerValue)) {
+            10.0f
+        } else if (naturalGasLanguages.contains(spinnerValue)) {
+            12.0f
+        } else {
+            0.0f
+        }
+    }
+
+    private fun createAlert(title: String, message: String) {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle(title)
+        alert.setMessage(message)
+        alert.show()
+    }
+
+    private fun  validateConsumption(spinnerItem : String, consumption: Float?, textInput: TextInputLayout): Boolean {
+        if (customLanguages.contains(spinnerItem) && consumption == null) {
+            textInput.requestFocus()
+            createAlert(getString(R.string.comsumption_not_found), getString(R.string.consumption_error_message))
+            return false
+        }
+        return true
+    }
+
+    private fun validateCost(cost : Float?, textInput: TextInputLayout): Boolean {
+        if (cost == null) {
+            textInput.requestFocus()
+            createAlert(getString(R.string.cost_not_found),getString(R.string.cost_error))
+            return false
+        }
+        return true
+    }
 }
+
